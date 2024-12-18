281134fcd0011f5061ff372b6ed9ff1002ce7ab8
Wh0Rv <salsabilacf04@icloud.com>
2024-12-19 01:38:38 +0700

    logout flutter

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:yummyogya_mobile/dashboard/screens/dashboard_screen.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:yummyogya_mobile/screens/login.dart';
+
 class LeftDrawer extends StatelessWidget {
   final String username; // Parameter untuk nama pengguna
 
   const LeftDrawer({Key? key, required this.username}) : super(key: key);
+
+  // Fungsi logout
+  Future<void> logout(BuildContext context) async {
+    const String logoutUrl = 'http://127.0.0.1:8000/authentication/logout_flutter/';
+
+    try {
+      final response = await http.post(Uri.parse(logoutUrl));
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        if (data['status'] == true) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(data['message'])),
+          );
+
+          // Navigasi ke halaman login setelah logout
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Logout gagal: ${data['message']}")),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Error: ${response.statusCode}")),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Terjadi kesalahan: $e")),
+      );
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -96,12 +137,8 @@ class LeftDrawer extends StatelessWidget {
           ListTile(
             leading: const Icon(Icons.logout),
             title: const Text('Logout'),
-            onTap: () {
-              // Logika logout
-              Navigator.pop(context); // Tutup drawer
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Anda telah logout')),
-              );
+            onTap: () async {
+              await logout(context);
             },
           ),
         ],
