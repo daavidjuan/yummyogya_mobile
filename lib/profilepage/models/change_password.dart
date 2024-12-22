d17f483ce53715995d22cba1888ae2e6b51a6aa2
Wh0Rv <salsabilacf04@icloud.com>
2024-12-22 15:19:32 +0700

    update profile & change pass

---
+++ lib/profilepage/models/change_password.dart
@@ -0,0 +1,195 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ChangePasswordModal extends StatefulWidget {
+  final String baseUrl;
+  final String username;
+
+  const ChangePasswordModal({
+    super.key,
+    required this.baseUrl,
+    required this.username,
+  });
+
+  @override
+  ChangePasswordModalState createState() => ChangePasswordModalState();
+}
+
+class ChangePasswordModalState extends State<ChangePasswordModal> {
+  final TextEditingController _oldPasswordController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  bool _isSubmitting = false;
+
+  Future<void> _changePassword() async {
+    if (!mounted) return;
+
+    if (_newPasswordController.text != _confirmPasswordController.text) {
+      _showSnackbar('Passwords do not match', Colors.red);
+      return;
+    }
+
+    setState(() {
+      _isSubmitting = true;
+    });
+
+    try {
+      final response = await http.post(
+        Uri.parse('${widget.baseUrl}/profilepage/change-password/api'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'username': widget.username,
+          'old_password': _oldPasswordController.text,
+          'new_password1': _newPasswordController.text,
+          'new_password2': _confirmPasswordController.text,
+        }),
+      );
+
+      final responseData = jsonDecode(response.body);
+
+      if (mounted) {
+        if (response.statusCode == 200 && responseData['success']) {
+          _showSnackbar(responseData['message'], Colors.green);
+          Navigator.pop(context, true);
+        } else {
+          _showSnackbar(responseData['message'], Colors.red);
+        }
+      }
+    } catch (e) {
+      if (mounted) {
+        _showSnackbar('An error occurred: $e', Colors.red);
+      }
+    } finally {
+      if (mounted) {
+        setState(() {
+          _isSubmitting = false;
+        });
+      }
+    }
+  }
+
+  void _showSnackbar(String message, Color color) {
+    if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            message,
+            style: const TextStyle(color: Colors.white),
+          ),
+          backgroundColor: color,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(
+        left: 16,
+        right: 16,
+        bottom: MediaQuery.of(context).viewInsets.bottom + 16,
+        top: 16,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Change Password',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: _oldPasswordController,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Old Password',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.orange),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.deepOrange),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: _newPasswordController,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'New Password',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.orange),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.deepOrange),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: _confirmPasswordController,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Confirm Password',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.orange),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.deepOrange),
+              ),
+            ),
+          ),
+          const SizedBox(height: 24),
+          _isSubmitting
+              ? const Center(child: CircularProgressIndicator())
+              : ElevatedButton(
+                  onPressed: _changePassword,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
+                      horizontal: 24,
+                    ),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Submit',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+        ],
+      ),
+    );
+  }
+}
