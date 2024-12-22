e9abba98e775967cc0f9abeb9b5eec512f73763a
kiyyawww <woydoy18@gmail.com>
2024-12-22 20:36:01 +0700

    update notes 2

--- lib/wishlist/models/wishlist_product.dart
+++ lib/wishlist/models/wishlist_product.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 
 List<WishlistProduct> wishlistProductFromJson(String str) =>
-    List<WishlistProduct>.from(
-        json.decode(str).map((x) => WishlistProduct.fromJson(x)));
+    List<WishlistProduct>.from(json.decode(str).map((x) => WishlistProduct.fromJson(x)));
 
 String wishlistProductToJson(List<WishlistProduct> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
@@ -23,31 +22,10 @@ class WishlistProduct {
     required this.deskripsi,
     required this.rating,
     required this.gambar,
-    required this.notes,
+    this.notes = "",
   });
 
-  WishlistProduct copyWith({
-    int? id,
-    String? nama,
-    String? gambar,
-    String? rating,
-    int? harga,
-    String? notes,
-    String? deskripsi,
-  }) {
-    return WishlistProduct(
-      id: id ?? this.id,
-      nama: nama ?? this.nama,
-      gambar: gambar ?? this.gambar,
-      rating: rating ?? this.rating,
-      harga: harga ?? this.harga,
-      notes: notes ?? this.notes,
-      deskripsi: deskripsi ?? this.deskripsi,
-    );
-  }
-
-  factory WishlistProduct.fromJson(Map<String, dynamic> json) =>
-      WishlistProduct(
+  factory WishlistProduct.fromJson(Map<String, dynamic> json) => WishlistProduct(
         id: json["id"],
         nama: json["nama"],
         harga: json["harga"],
@@ -66,4 +44,24 @@ class WishlistProduct {
         "gambar": gambar,
         "notes": notes,
       };
+
+    WishlistProduct copyWith({
+    int? id,
+    String? nama,
+    int? harga,
+    String? deskripsi,
+    String? rating,
+    String? gambar,
+    String? notes,
+  }) {
+    return WishlistProduct(
+      id: id ?? this.id,
+      nama: nama ?? this.nama,
+      harga: harga ?? this.harga,
+      deskripsi: deskripsi ?? this.deskripsi,
+      rating: rating ?? this.rating,
+      gambar: gambar ?? this.gambar,
+      notes: notes ?? this.notes,
+    );
+  }
 }
